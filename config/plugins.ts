a06d895bde032768c02c967029034f9ce41d5bf2
Mohammed Bajaman <mohammed_bajaman@hotmail.com>
2025-02-25 23:14:39 -0800

    Added aws s3 provider to strapi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,29 @@
-export default () => ({});
+export default () => ({
+
+    upload: {
+        config: {
+            provider: 'aws-s3',
+            providerOptions: {
+                // baseUrl: process.env.CDN_URL,
+                // rootPath: process.env.CDN_ROOT_PATH,
+                s3Options: {
+                    credentials: {
+                        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                        secretAccessKey: process.env.AWS_ACCESS_SECRET,
+                    },
+                    region: process.env.AWS_REGION,
+                    params: {
+                        ACL: process.env.AWS_ACL || 'public-read',
+                        signedUrlExpires: process.env.AWS_SIGNED_URL_EXPIRES || 15 * 60,
+                        Bucket: process.env.AWS_BUCKET_NAME,
+                    },
+                },
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
+            },
+        }
+    }
+});
